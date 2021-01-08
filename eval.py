1a37feb197241badabba5ec4d4afc9c674be0ab3
Nirmalya Misra <nirmalya14misra@gmail.com>
2021-01-09 03:43:04 +0530

    Updated eval.py

--- eval.py
+++ eval.py
@@ -1,6 +1,24 @@
 class evaluate:
+    
     def calc():
-        a = int(input("Number "))
+        import numpy 
+        import math
+        a = (input("Number "))
+        try:
+            a = int(a)
+        except Exception as e:
+            s = input("Expression ")
+            c = compile(s,"<string>","eval")
+            x =""
+            #try:
+            res = eval(c)
+            print(res)
+            #except Exception as E:
+            #   print("Wrong input somewhere, please check the instructions again!")
+            #    x = "wrong input"
+            #    return x
+            return res
+
         s = input("Expression ")
         s = s.replace("a",str(a))
         c = compile(s,"<string>","eval")
